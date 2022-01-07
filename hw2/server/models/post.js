e95328936d787f10a899f16c86de629b1f4d42a2
Hauryliuk-Viktoria <81378781+Hauryliuk-Viktoria@users.noreply.github.com>
2022-01-07 20:54:16 +0300

    Add files via upload

--- hw2/server/models/post.js
+++ hw2/server/models/post.js
@@ -1,1 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+
+const schema = new Schema({
+    name: { type: String, required: true },
+    body: { type: String, required: true },
+    excerpt: String,
+    categories: {
+        type: [mongoose.Types.ObjectId],
+        ref: 'Category',
+    },
+    tags: {
+        type: [mongoose.Types.ObjectId],
+        ref: 'Tag',
+    },
+    author: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Author',
+    },
+   // timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }  
+})
+
+module.exports = mongoose.model('Post', schema)
+
 
---
+++ hw2/server/models/tag.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+
+const schema = new Schema({
+    name: { type: String, required: true },
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+})
+
+module.exports = mongoose.model('Tag', schema)
