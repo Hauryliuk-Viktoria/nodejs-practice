a9f05318f5e585b60f3d267c07c0ecfc5635f825
Hauryliuk-Viktoria <81378781+Hauryliuk-Viktoria@users.noreply.github.com>
2022-01-07 20:49:35 +0300

    Add files via upload

--- hw2/server/index.js
+++ hw2/server/index.js
@@ -1,1 +1,33 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
+const postsRoutes = require('./routes/posts')
+const categoriesRoutes = require('./routes/categories')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+//сюда добавить middleware
+
+app.use(postsRoutes)
+
+//app.use(categoriesRoutes)
+
+app.get('/', (req, res) => {
+    res.send('Homepage')
+})
+
+const url = 'mongodb+srv://root:<password>@cluster0.gj5xw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(url, (error) => {
+    if (error) {
+        console.log(error)
+        return
+    }
+
+    console.log('It is connected')
+    app.listen(3000, () => {
+        console.log('It is running')
+    })
+})
