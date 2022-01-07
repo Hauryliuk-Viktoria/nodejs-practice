4cd04805af193be17eb3279dc295867177a649a3
Hauryliuk-Viktoria <81378781+Hauryliuk-Viktoria@users.noreply.github.com>
2022-01-07 20:50:44 +0300

    Add files via upload

--- hw2/client/index.js
+++ hw2/client/index.js
@@ -1,1 +1,48 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const handlebars = require('express-handlebars'); 
 
+const { connect, disconnect } = require('./database')
+
+const postsRoutes = require('./routes/posts')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use(postsRoutes)
+
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs',
+}))
+
+app.get('/', (req, res) => {
+    res.send('Homepage')
+})
+
+app.get('/disconnect', (req, res) => {
+    disconnect(() => {
+        res.send('The MongoDB connection was closed')
+    })
+})
+
+
+connect((error) => {
+    if (error) {
+        console.log(error)
+    }
+
+    app.listen(3000, () => {
+        console.log('It is running')
+    })
+})
+
+
+// todo: check callback version
+// client.connect((client) => {
+//     console.log(client)
+
+//     app.listen(3000, () => {
+//         console.log('It is running')
+//     })
+// })
