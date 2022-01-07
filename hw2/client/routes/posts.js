202fabb7a3ac3c0956d1a1a53c4ac89b52558404
Hauryliuk-Viktoria <81378781+Hauryliuk-Viktoria@users.noreply.github.com>
2022-01-07 20:55:40 +0300

    Add files via upload

--- hw2/client/routes/posts.js
+++ hw2/client/routes/posts.js
@@ -1,1 +1,35 @@
+const express = require('express');
 
+const { getDb } = require('../database');
+
+const router = express.Router();
+
+router.get('/posts', (req, res) => {
+    const db = getDb();
+    db
+        .collection('posts')
+        .find()
+        .toArray()
+        .then((posts) => {
+            res.json(posts)
+        })
+        .catch((error) => {
+            res.status(400).json({ error: error.message })
+        })
+})
+
+router.post('/posts', (req, res) => {
+    const db = getDb();
+    db
+        .collection('posts')
+        .insertOne(req.body)
+        .then((data) => {
+            console.log(data)
+            res.json({ message: 'The post was created' })
+        })
+        .catch((error) => {
+            res.status(400).json({ error: error.message })
+        })
+})
+
+module.exports = router
