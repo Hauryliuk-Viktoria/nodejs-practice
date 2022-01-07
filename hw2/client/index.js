eed4d41474279ca44e1e4b6f6fab1e9e391146a9
Hauryliuk-Viktoria <81378781+Hauryliuk-Viktoria@users.noreply.github.com>
2022-01-07 21:45:59 +0300

    Add files via upload

--- hw2/client/index.js
+++ hw2/client/index.js
@@ -16,8 +16,57 @@ app.engine('hbs', handlebars.engine({
     extname: '.hbs',
 }))
 
+app.set('views', './views');
+
 app.get('/', (req, res) => {
     res.send('Homepage')
+})
+
+app.get('/posts', (req, res) => {
+    const posts = [
+        {
+            name: 'Post 1',
+            body: 'Some description 1',
+            categories: 'Category 1',
+            tags: 'Tag 1',
+            author: 'Some author 1'},
+        {
+            name: 'Post 2',
+            body: 'Some description 2',
+            categories: 'Category 2',
+            tags: 'Tag 2',
+            author: 'Some author 2'},
+        {
+            name: 'Post 3',
+            body: 'Some description 3',
+            categories: 'Category 3',
+            tags: 'Tag 3',
+            author: 'Some author 3'
+        }
+    ]
+
+    const havePosts = !!posts.length;
+    console.log(havePosts)
+
+    res.render('posts', { posts, total: posts.length, havePosts })
+})
+
+app.post('/post', (req, res) => {
+
+    const chunks = [];
+
+    req.on('data', (chunk) => {
+        chunks.push(chunk);
+    })
+
+    req.on('end', () => {
+
+        const data = JSON.parse(Buffer.concat(chunks).toString());
+
+        console.log(data, data.key);
+
+        res.send('It is ok')
+    });
 })
 
 app.get('/disconnect', (req, res) => {
@@ -36,13 +85,3 @@ connect((error) => {
         console.log('It is running')
     })
 })
-
-
-// todo: check callback version
-// client.connect((client) => {
-//     console.log(client)
-
-//     app.listen(3000, () => {
-//         console.log('It is running')
-//     })
-// })
